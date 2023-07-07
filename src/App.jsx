17b1e560c950be8bbc861b058f0235d7c45a7088
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-06 22:47:55 -0300

    Inserindo Input's

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 export function App() {
+
+  /* YARN DEV */
+
   return (
-    <h1>Hello World!</h1>
+    <div className="container">
+      <header>
+        <img src="" alt="" />
+        <span>Merci d'entrer vos informations de connexion</span>
+      </header>
+      <form>
+        <div className="inputContainer">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" placeholder="joao@mail.com"/>
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="password">Mot de passe</label>
+          <input type="password" name="password" id="password" placeholder="**************"/>
+        </div>
+        <div></div>
+      </form>
+    </div>
   )
 }
